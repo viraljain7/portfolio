68f87bb1b453cece1c3cd533cafc988179b8373f
viraljain7 <veerljain1234@gmail.com>
2024-05-18 11:25:13 +0530

    project setup done

--- app/page.tsx
+++ app/page.tsx
@@ -1,5 +1,9 @@
 import Image from "next/image";
 
 export default function Home() {
-  return <>Portfolio</>;
+  return (
+    <main className="bg-black-100 relative flex justify-center items-center flex-col overflow-hidden mx-auto sm:px-10 px-5">
+      <div className="max-w-7xl w-full"></div>
+    </main>
+  );
 }
